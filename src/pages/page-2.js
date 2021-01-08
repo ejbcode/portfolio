f51a6cf92c3dd847850fb35f600be30b87213b55
Ernesto B <ejbcode@gmail.com>
2021-01-08 15:40:22 -0300

    working in client-side fetch demo

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { graphql, Link } from 'gatsby';
 
 import Layout from '../components/layout';
@@ -6,19 +6,44 @@ import SEO from '../components/seo';
 import Image from '../components/image';
 import InstaGrid from '../components/InstaGrid';
 
-const SecondPage = ({ data }) => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people❤</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/">Go home</Link> <br />
-    <InstaGrid insta={data.insta.nodes} />
-  </Layout>
-);
+const SecondPage = ({ data }) => {
+  const [starsCount, setStarsCount] = useState([]);
+  useEffect(() => {
+    // get data from GitHub api
+    fetch(`
+    https://www.instagram.com/graphql/query/?query_hash=42323d64886122307be10013ad2dcc44&variables={"id":519208,"first":5}`)
+      .then(response => response.json()) // parse JSON from request
+      .then(resultData => {
+        setStarsCount(resultData.data.user.edge_owner_to_timeline_media.edges);
+      }); // set data for the number of stars
+  }, []);
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people❤</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/">Go home</Link> <br />
+      <InstaGrid insta={data.insta.nodes} />
+      {/* <p>this repo have {starsCount} stars</p> */}
+      <div>
+        {starsCount.map(post => {
+          console.log(post.node.edge_media_to_caption.edges[0].node.text);
+          return (
+            <img
+              src={post.node.thumbnail_resources[0].src}
+              alt={post.node.edge_media_to_caption.edges[0].node.text}
+            />
+          );
+        })}
+      </div>
+      {/* <pre>{JSON.stringify(starsCount, null, 4)}</pre> */}
+    </Layout>
+  );
+};
 
 export default SecondPage;
 
